8b02c6d1345721114b27076d045d8cf01b70dbeb
konfle <konrad.flejter@gmail.com>
2023-09-12 10:03:11 +0200

    Add update movie by movie id.

--- movie_service/app/main.py
+++ movie_service/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 
@@ -37,3 +37,14 @@ async def add_movie(payload: Movie):
     movie = dict(payload)
     fake_movie_db.append(movie)
     return {'id': len(fake_movie_db) - 1}
+
+
+@app.put('/{id}')    # Where movie_id is an index of fake_movie_db
+async def update_movie(movie_id: int, payload: Movie):
+    movie = dict(payload)
+    movies_length = len(fake_movie_db)
+    if 0 <= movie_id <= movies_length:
+        fake_movie_db[movie_id] = movie
+        return None
+    raise HTTPException(status_code=404,
+                        detail="Movie with given id not found")
