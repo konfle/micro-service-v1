679179a3948c87cd4bb3f585a184372d63bf7e08
konfle <konrad.flejter@gmail.com>
2023-09-12 10:04:39 +0200

    Added delete endpoint.

--- movie_service/app/main.py
+++ movie_service/app/main.py
@@ -39,7 +39,7 @@ async def add_movie(payload: Movie):
     return {'id': len(fake_movie_db) - 1}
 
 
-@app.put('/{id}')    # Where movie_id is an index of fake_movie_db
+@app.put('/{id}')  # Where movie_id is an index of fake_movie_db
 async def update_movie(movie_id: int, payload: Movie):
     movie = dict(payload)
     movies_length = len(fake_movie_db)
@@ -48,3 +48,12 @@ async def update_movie(movie_id: int, payload: Movie):
         return None
     raise HTTPException(status_code=404,
                         detail="Movie with given id not found")
+
+
+@app.delete('/{id}')  # Where movie_id is an index of fake_movie_db
+async def delete_movie(movie_id: int):
+    movies_length = len(fake_movie_db)
+    if 0 <= movie_id <= movies_length:
+        del fake_movie_db[movie_id]
+        return None
+    raise HTTPException(status_code=404, detail="Movie with given id not found")
