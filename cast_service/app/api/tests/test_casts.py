967c763d40eec5d186d92d2f57cac0d6f3427b5a
konfle <konrad.flejter@gmail.com>
2023-09-16 11:21:42 +0200

    Refactor of create cast test.

--- cast_service/app/api/tests/test_casts.py
+++ cast_service/app/api/tests/test_casts.py
@@ -6,16 +6,15 @@ from fastapi.testclient import TestClient
 from app.api import db_manager as dbm
 
 
-def test_create_cast(test_app, monkeypatch):
+def test_create_cast(test_app, mock_add_cast):
     test_request_payload = {"name": "Jane Doe", "nationality": "American"}
-    test_response_payload = {"name": "Jane Doe", "nationality": "American", "id": 1}
+    test_response_payload = {
+        "name": "Jane Doe",
+        "nationality": "American",
+        "id": 1
+    }
 
-    async def mock_add_cast(payload):
-        return 1
-
-    monkeypatch.setattr(dbm, "add_cast", mock_add_cast)
-
-    response = test_app.post("", content=json.dumps(test_request_payload), )
+    response = test_app.post("", json=test_request_payload)
 
     assert response.status_code == 201
     assert response.json() == test_response_payload
