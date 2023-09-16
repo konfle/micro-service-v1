9107da70c23022475644a89056030b77717e3452
konfle <konrad.flejter@gmail.com>
2023-09-16 11:22:07 +0200

    Add moc fixture for mock add cast method.

--- cast_service/app/api/tests/conftest.py
+++ cast_service/app/api/tests/conftest.py
@@ -3,9 +3,17 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app.main import app
+from app.api import db_manager as dbm
 
 
 @pytest.fixture(scope="module")
 def test_app():
     with TestClient(app, base_url=f"http://localhost:8080/api/v1/casts/") as client:
         yield client
+
+
+@pytest.fixture
+def mock_add_cast(monkeypatch):
+    async def mock_add_cast(payload):
+        return 1
+    monkeypatch.setattr(dbm, "add_cast", mock_add_cast)
