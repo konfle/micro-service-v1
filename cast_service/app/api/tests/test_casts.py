96571fc7e78f10095e79be0818a92b368211009a
konfle <konrad.flejter@gmail.com>
2023-09-16 11:15:49 +0200

    Add test for adding cast.

--- cast_service/app/api/tests/test_casts.py
+++ cast_service/app/api/tests/test_casts.py
@@ -1,4 +1,24 @@
+import json
+import pytest
+
 from fastapi.testclient import TestClient
+
+from app.api import db_manager as dbm
+
+
+def test_create_cast(test_app, monkeypatch):
+    test_request_payload = {"name": "Jane Doe", "nationality": "American"}
+    test_response_payload = {"name": "Jane Doe", "nationality": "American", "id": 1}
+
+    async def mock_add_cast(payload):
+        return 1
+
+    monkeypatch.setattr(dbm, "add_cast", mock_add_cast)
+
+    response = test_app.post("", content=json.dumps(test_request_payload), )
+
+    assert response.status_code == 201
+    assert response.json() == test_response_payload
 
 
 def test_get(test_app):
