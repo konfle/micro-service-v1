891be5b3bc501a6fefda4b8e4fb0100eaf2a89c6
konfle <konrad.flejter@gmail.com>
2023-09-16 11:34:54 +0200

    Add more test cases for create cast.

--- cast_service/app/api/tests/test_casts.py
+++ cast_service/app/api/tests/test_casts.py
@@ -20,8 +20,37 @@ def test_create_cast(test_app, mock_add_cast):
     assert response.json() == test_response_payload
 
 
+def test_create_cast_without_nationality(test_app, mock_add_cast):
+    test_request_payload = {"name": "Jane Doe"}
+    test_response_payload = {
+        "name": "Jane Doe",
+        "nationality": None,
+        "id": 1
+    }
+
+    response = test_app.post("", json=test_request_payload)
+
+    assert response.status_code == 201
+    assert response.json() == test_response_payload
+
+
+def test_create_cast_without_name(test_app, mock_add_cast):
+    response = test_app.post("", json={"nationality": "American"})
+    assert response.status_code == 422
+
+
+def test_create_cast_with_wrong_name_data_type(test_app, mock_add_cast):
+    response = test_app.post("", json={"name": 2})
+    assert response.status_code == 422
+
+
+def test_create_cast_with_wrong_nationality_data_type(test_app, mock_add_cast):
+    response = test_app.post("", json={"nationality": 2})
+    assert response.status_code == 422
+
+
 def test_get(test_app):
-    response = test_app.get(f"1/")
+    response = test_app.get("1/")
     assert response.status_code == 200
     assert response.json() == {
         "name": "Daisy Ridley",
