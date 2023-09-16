8561da4c837970dd7ca18326f14a0525d5f5c6ac
konfle <konrad.flejter@gmail.com>
2023-09-16 12:16:38 +0200

    Update test_app fixture for cast service.

--- cast_service/app/api/tests/conftest.py
+++ cast_service/app/api/tests/conftest.py
@@ -8,6 +8,23 @@ from app.api import db_manager as dbm
 
 @pytest.fixture(scope="module")
 def test_app():
+    """
+    Pytest fixture providing a TestClient for the FastAPI app.
+
+    This fixture sets up a TestClient for the FastAPI app to simulate HTTP requests
+    during testing. It configures the client to use the specified base URL for API
+    endpoints under '/api/v1/casts/'.
+
+    Usage:
+    ```
+    def test_example(test_app):
+        response = test_app.get("/some_endpoint")
+        assert response.status_code == 200
+    ```
+
+    Returns:
+        TestClient: A TestClient instance for making HTTP requests to the app.
+    """
     with TestClient(app, base_url=f"http://localhost:8080/api/v1/casts/") as client:
         yield client
 
